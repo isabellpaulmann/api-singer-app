d9064abe5451978988b13ebdd2e36a860f001f4d
Kim Klinkhammer <kim.klinkhammer@rwth-aachen.de>
2022-05-27 15:32:28 +0200

    Add

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world!');
+fetch ('http://hp-api.herokuapp.com/api/characters/staff').then 
+(response =>
+response.json()).then 
+(harryData).catch (error => {error.message})
+console.clear()
+function harryData (data) {
+    console.log(data[0].name)
+    const harryStaffs = data
+    const list = document.createElement('ul')
+    list.setAttribute('role','list')
+    document.body.append(list)
+    harryStaffs.forEach (harryStaff  => {
+        const listItem = document.createElement('li')
+        listItem.className = 'card'
+        listItem.innerHTML = `${data[0].name}`
+        list.append(listItem)
+    })
+
+}
